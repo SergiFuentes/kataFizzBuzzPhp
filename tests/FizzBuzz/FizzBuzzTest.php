ee9d8f0c0e02dbdc01156ddef5abdbe23c825cd4
GranainoBUAB <josemiguel@quesada.eu>
2022-01-18 17:11:51 +0100

    first commit

---
+++ tests/FizzBuzz/FizzBuzzTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests;
+
+use App\FizzBuzz\Fizzbuzz;
+use PHPUnit\Framework\TestCase;
+
+
+class FizzBuzzTest extends TestCase {
+
+	public function test_example(
+		) {
+	
+			$this->assertEquals(4, 4);
+		}
+
+
+}
+
+
