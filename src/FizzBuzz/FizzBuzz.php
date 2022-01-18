16375541163478c773e22d098a775ffa06c5b35d
SergiFuentes <you@example.com>
2022-01-18 22:22:56 +0100

    FizzBuzz swhitch and ternary conditional

--- src/FizzBuzz/FizzBuzz.php
+++ src/FizzBuzz/FizzBuzz.php
@@ -3,5 +3,40 @@ namespace App\FizzBuzz;
 
 class Fizzbuzz{
     
+    public function execute($num){
+
+        /* if($num % 15 == 0 ){
+            return 'FizzBuzz';
+        }
+        
+        if($num % 3 == 0){
+            return 'Fizz';
+        }
+        if($num % 5 == 0){
+            return 'Buzz';
+        }
+
+        return $num; */
+
+       /*  switch  ($num) {
+            case ($num % 15 ===0):
+                return 'FizzBuzz';
+                break;
+            case ($num % 5 ===0):
+                return 'Buzz';
+                break;
+            case ($num % 3 ===0):
+                return 'Fizz';
+                break;
+            default:
+                return $num;
+
+        } */
+
+       return $num % 15 === 0 ? 'FizzBuzz' :( $num % 5 === 0 ? 'Buzz' : ($num % 3 === 0 ? 'Fizz' : $num));
+
+        
+
+    }
     
 }
--- tests/FizzBuzz/FizzBuzzTest.php
+++ tests/FizzBuzz/FizzBuzzTest.php
@@ -13,6 +13,47 @@ class FizzBuzzTest extends TestCase {
 	
 			$this->assertEquals(4, 4);
 		}
+	public function test_return_Fizz_if_divisible_by_3(){
+		//GIVEN
+		$fizzBuzz = new FizzBuzz();
+		//WHEN
+		$return = $fizzBuzz->execute(3);
+		//THEN
+		$this->assertEquals('Fizz',$return);
+	}
+
+	public function test_return_Buzz_if_divisible_by_5(){
+		//GIVEN
+		$fizzBuzz = new FizzBuzz();
+		//WHEN
+		$return = $fizzBuzz->execute(5);
+		//THEN
+		$this->assertEquals('Buzz',$return);
+	}
+
+	public function test_return_FizzBuzz_if_divisible_by_3_and_5(){
+		//GIVEN
+		$fizzBuzz = new FizzBuzz();
+		//WHEN
+		$return = $fizzBuzz->execute(15);
+		//THEN
+		$this->assertEquals('FizzBuzz',$return);
+	}
+
+	public function test_return_number_if_not_divisible_by_3_or_5(){
+		//GIVEN
+		$fizzBuzz = new FizzBuzz();
+		//WHEN
+		$return = $fizzBuzz->execute(4);
+		//THEN
+		$this->assertEquals(4,$return);
+	}
+
+
+
+
+
+
 
 
 }
